e0c30c58de12aa2bcc19d9bd1447d7f7da93c8be
amitsachdeva45 <amitsachdeva45@gmail.com>
2019-07-28 18:14:06 -0400

    Add console statements in test cases

--- Projects/src/test/GammaAllTest.java
+++ Projects/src/test/GammaAllTest.java
@@ -36,64 +36,76 @@ public class GammaAllTest {
 
   @Test
   public void testLargePositiveNumber1() {
+    System.out.println("Testing Large Positive number = 110.0");
     assertEquals("infinity", gammaModel.startGamma("110.0"));
   }
   
   @Test
   public void testLargePositiveNumber2() {
+    System.out.println("Testing Large Positive number = 130.0");
     assertEquals("infinity", gammaModel.startGamma("130.0"));
   }
   
   
   @Test
   public void testNegativeNumber1() {
+    System.out.println("Testing negative number = -2.0");
     assertEquals("negative", gammaModel.startGamma("-2.0"));
   }
   
   
   @Test
   public void testNegativeNumber2() {
+    System.out.println("Testing boundary negative input = -0.0");
     assertEquals("1.0", gammaModel.startGamma("-0.0"));
   }
   
   
   @Test
   public void testZeroValue() {
+    System.out.println("Testing zero value");
     assertEquals("1.0", gammaModel.startGamma("0.0"));
   }
   
   @Test
   public void testNormalPositiveNumber1() {
+    System.out.println("Positive value in a domain = 13.0");
     assertEquals("479001600.000", gammaModel.startGamma("13.0"));
   }
   
   @Test
   public void testNormalPositiveNumber2() {
+    System.out.println("Positive value in a domain = 6.0");
     assertEquals("120.000", gammaModel.startGamma("6.0"));
   }
   
   @Test
   public void testKnownConstant() {
+    System.out.println("Testing already defined value = 0.5");
     assertEquals("1.710", gammaModel.startGamma("0.5"));
   }  
   
   @Test
   public void testWrongInput1() {
+    System.out.println("Testing wrong input of complex number = i9");
     assertEquals("error", gammaModel.startGamma("i9"));
   } 
   
   @Test
   public void testWrongInput2() {
+    System.out.println("Testing any random wrong input = gjhfjjf ");
     assertEquals("error", gammaModel.startGamma("gjhfjjf"));
   } 
   
   @Test
-  public void testComplexNumber1() {
+  public void testComplexNumber1() { 
+    System.out.println("Testing correct complex number = 0+i9");
     assertEquals("1.0", gammaModel.startGamma("0 + i9"));
   } 
   
   @Test
   public void testComplexNumber2() {
+    System.out.println("Testing correct complex number = 6+i9");
     assertEquals("120.000", gammaModel.startGamma("6 + i9"));
   } 
 
